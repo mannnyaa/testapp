8b18abb658e09c168c2553a9a0162a9664e6e86a
mannnyaa <mashalitzman9@gmail.com>
2025-06-28 18:27:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,14 @@ function fcFrameMeta(): Plugin {
     transformIndexHtml(html: string) {
       const config = {
         version: 'next',
-        imageUrl: 'TUNNEL/logo.png',
+        imageUrl: 'testapp-ruddy.vercel.app/logo.png',
         button: {
           title: '🚩 Start',
           action: {
             type: 'launch_frame',
             name: 'HelloWorld',
-            url: `TUNNEL/?v=${appVersion}`, 
-            splashImageUrl: 'TUNNEL/logo.png'
+            url: `testapp-ruddy.vercel.app/?v=${appVersion}`, 
+            splashImageUrl: 'testapp-ruddy.vercel.app/logo.png'
           },
         },
       };
@@ -37,7 +37,7 @@ export default defineConfig({
   server: {
     port: 3000,
     allowedHosts: [
-      '5abe318fed171fa07244be15c4e93881.serveo.net'
+      'testapp-ruddy.vercel.app'
     ],
     headers: {
       'X-Frame-Options': 'ALLOWALL',
